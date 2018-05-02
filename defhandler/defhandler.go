d47e73b2647f16b33828fc9d22aeb850c20ed4ed
Kurnia D Win <kurnia.d.win@gmail.com>
2018-05-02 13:20:54 +0700

    remove unecessary default handler

--- defhandler/defhandler.go
+++ defhandler/defhandler.go
@@ -4,29 +4,18 @@ package defhandler
 import "net/http"
 
 var (
-	StatusBadRequest                 = ResponseCode(http.StatusBadRequest)
-	StatusUnauthorized               = ResponseCode(http.StatusUnauthorized)
-	StatusForbidden                  = ResponseCode(http.StatusForbidden)
-	StatusNotFound                   = ResponseCode(http.StatusNotFound)
-	StatusMethodNotAllowed           = ResponseCode(http.StatusMethodNotAllowed)
-	StatusNotAcceptable              = ResponseCode(http.StatusNotAcceptable)
-	StatusConflict                   = ResponseCode(http.StatusConflict)
-	StatusGone                       = ResponseCode(http.StatusGone)
-	StatusUnsupportedMediaType       = ResponseCode(http.StatusUnsupportedMediaType)
-	StatusUnprocessableEntity        = ResponseCode(http.StatusUnprocessableEntity)
-	StatusLocked                     = ResponseCode(http.StatusLocked)
-	StatusUnavailableForLegalReasons = ResponseCode(http.StatusUnavailableForLegalReasons)
+	StatusBadRequest           = ResponseCode(http.StatusBadRequest)
+	StatusUnauthorized         = ResponseCode(http.StatusUnauthorized)
+	StatusForbidden            = ResponseCode(http.StatusForbidden)
+	StatusNotFound             = ResponseCode(http.StatusNotFound)
+	StatusMethodNotAllowed     = ResponseCode(http.StatusMethodNotAllowed)
+	StatusUnsupportedMediaType = ResponseCode(http.StatusUnsupportedMediaType)
+	StatusUnprocessableEntity  = ResponseCode(http.StatusUnprocessableEntity)
 )
 
-func Error(error string, code int) http.HandlerFunc {
+func Error(err string, code int) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, error, code)
-	}
-}
-
-func NotFound() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		http.NotFound(w, r)
+		http.Error(w, err, code)
 	}
 }
 
@@ -40,20 +29,4 @@ func ResponseCode(code int) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(code)
 	}
-}
-
-func FileServer(fs http.FileSystem) http.HandlerFunc {
-	return http.FileServer(fs).ServeHTTP
-}
-
-func FileServerRelative(fs http.FileSystem) http.HandlerFunc {
-	panic("defhandler: implementation is not complete yet")
-}
-
-func FileServerDir(dir string) http.HandlerFunc {
-	return FileServer(http.Dir(dir))
-}
-
-func FileServerDirRelative(dir string) http.HandlerFunc {
-	return FileServerRelative(http.Dir(dir))
 }
