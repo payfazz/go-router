51dd7083160bb8f8aac2e6cffcc5f43ea87c6aba
Kurnia D Win <kurnia.d.win@gmail.com>
2020-03-10 09:31:48 +0700

    adding test example

--- v2/method.go
+++ v2/method.go
@@ -7,6 +7,6 @@ import (
 )
 
 // ByMethod will return handler for routing via method
-func (Router) ByMethod(handler HandlerMapping) http.HandlerFunc {
+func (router Router) ByMethod(handler HandlerMapping) http.HandlerFunc {
 	return ((v1method.H)(handler)).C()
 }
